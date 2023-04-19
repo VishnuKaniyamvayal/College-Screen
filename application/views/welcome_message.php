1017679ab527928def0ae2ebbdac7477c693fa7b
vishnu <trainee@gmail.com>
2023-04-19 13:32:54 +0530

    cleaned

---
+++ application/views/welcome_message.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>College Screen</title>
+	<link rel="stylesheet" href=<?php echo base_url("/assets/js/jquery.min.js") ?>>
+	<link rel="stylesheet" href=<?php echo base_url("/assets/css/bootstrap.min.css") ?>>
+	<link rel="stylesheet" href=<?php echo base_url("/assets/js/bootstrap.min.js") ?>>
+	<style>
+    /* Define styles for the header */
+    header {
+      background-color: #333;
+      color: #fff;
+      height: 80px;
+      padding: 10px;
+    }
+
+    /* Define styles for the logo */
+    header img {
+      height: 60px;
+    }
+  </style>
+</head>
+<body>
+
+<div id="container">
+	<!-- header -->
+	<header>
+    <img src="logo.png" alt="Logo">
+  </header>
+    <!-- /header -->
+
+</div>
+
+</body>
+</html>
